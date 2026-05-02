065c995240c610894e210df84e87062559befb95
Pavel Glebov <pglebov@ozon.ru>
2026-05-02 14:47:20 +0300

    Add integration tests for V1DeleteCourse endpoint

--- tests/Peerly.Core.IntegrationTests/Infrastructure/WebApplicationFactory.cs
+++ tests/Peerly.Core.IntegrationTests/Infrastructure/WebApplicationFactory.cs
@@ -21,6 +21,7 @@ using Peerly.Core.Api.Infrastructure.Configuration;
 using Peerly.Core.ApplicationServices.Extensions;
 using Peerly.Core.FileStorage.Extensions;
 using Peerly.Core.IntegrationTests.Features.V1.Courses.CreateCourse.Infrastructure;
+using Peerly.Core.IntegrationTests.Features.V1.Courses.DeleteCourse.Infrastructure;
 using Peerly.Core.Messaging.Extensions;
 using Peerly.Core.Persistence.Extensions;
 
@@ -60,6 +61,19 @@ public sealed class WebApplicationFactory : IAsyncDisposable
             });
 
         return new CreateCourseGrpcClient(channel);
+    }
+
+    public DeleteCourseGrpcClient DeleteCourseClient()
+    {
+        var handler = new GrpcWebHandler(GetTestServer().CreateHandler());
+        var channel = GrpcChannel.ForAddress(
+            "http://localhost",
+            new GrpcChannelOptions
+            {
+                HttpHandler = handler
+            });
+
+        return new DeleteCourseGrpcClient(channel);
     }
 
     public async Task StartAsync()
